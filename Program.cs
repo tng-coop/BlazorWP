2921ef4212c3903e305806df6f4822bb5125ae5e
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-17 19:22:30 +0900

    Register AntDesign services

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWP;
 using MudBlazor.Services;
 using PanoramicData.Blazor.Extensions;
+using AntDesign;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddPanoramicDataBlazor();
+builder.Services.AddAntDesign();
 
 await builder.Build().RunAsync();
