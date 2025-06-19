0d9af71c44b47e12d7256e25b85738bc1aaa57de
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-19 14:54:21 +0900

    Add delete and last updated features

--- wwwroot/js/storageUtils.js
+++ wwwroot/js/storageUtils.js
@@ -1,3 +1,20 @@
 window.blazorwpStorage = {
-  keys: () => Object.keys(window.localStorage)
+  keys: () => Object.keys(window.localStorage),
+  itemInfo: key => {
+    const value = window.localStorage.getItem(key);
+    let ts = window.localStorage.getItem(key + '_timestamp');
+    if (!ts && value) {
+      try {
+        const obj = JSON.parse(value);
+        if (obj && typeof obj === 'object' && obj.lastUpdated) {
+          ts = obj.lastUpdated;
+        }
+      } catch { }
+    }
+    return { value: value, lastUpdated: ts };
+  },
+  delete: key => {
+    window.localStorage.removeItem(key);
+    window.localStorage.removeItem(key + '_timestamp');
+  }
 };
