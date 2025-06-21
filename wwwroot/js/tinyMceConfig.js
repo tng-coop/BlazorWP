87692c60175aa25bf456b25ccdb9a22609bf8e09
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-21 17:25:54 +0900

    Add info button to TinyMCE toolbar

--- wwwroot/js/tinyMceConfig.js
+++ wwwroot/js/tinyMceConfig.js
@@ -2,12 +2,20 @@ window.myTinyMceConfig = {
   promotion: false,
   branding: false,
   statusbar: false,
-  toolbar: 'undo redo | bold italic | customButton',
+  toolbar: 'undo redo | bold italic | customButton showInfoButton',
   setup: function (editor) {
     editor.ui.registry.addButton('customButton', {
       text: 'Alert',
       onAction: function () {
         alert('Hello from TinyMCE!');
+      }
+    });
+    editor.ui.registry.addButton('showInfoButton', {
+      text: 'Info',
+      onAction: function () {
+        const endpoint = localStorage.getItem('wpEndpoint') || '(none)';
+        const token = localStorage.getItem('jwtToken') || '(none)';
+        alert(`Endpoint: ${endpoint}\nJWT: ${token}`);
       }
     });
   }
